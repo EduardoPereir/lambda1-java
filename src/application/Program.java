c3399b45ccbf9bfbc5170482171d700f8f3bd84b
Eduardo Pereira Filho <eduardopereirafilho6@gmail.com>
2022-10-19 08:00:07 -0300

    Anonymous class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -15,13 +16,18 @@ public class Program {
         Product p2 = new Product("Monitor", 240.99);
         Product p3 = new Product("Keyboard", 99.90);
 
-        MyComparator mc = new MyComparator();
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+            }
+        };
 
         list.add(p1);
         list.add(p2);
         list.add(p3);
 
-        list.sort(mc);
+        list.sort(comp);
 
         for( Product l: list){
         System.out.println(l);
